40daeaebf43f8677bda200109ec6b898dc96dbb3
GLA7479 <18ERAN@GMAIL.COM>
2025-08-23 17:58:32 +0300

    Auto update and deploy

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,27 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/logo.png" />
+        {/* PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme */}
+        <meta name="theme-color" content="#0A0F1A" />
+
+        {/* iOS - run as web app when added to Home Screen */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="MLEO Games" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+        {/* Safe-area & full height on iOS */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+
+        {/* Icons */}
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
